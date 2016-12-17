2e31fc0802b037128fd5bc1ce7c2affbf40c021d
Sergey Shambir <sshambir@yandex.ru>
2016-12-17 17:02:11 +0300

    Добавил ресурсы

--- src/TmxLevel.h
+++ src/TmxLevel.h
@@ -5,6 +5,11 @@
 #include <map>
 #include <SFML/Graphics.hpp>
 
+// В картах TMX объект - это область на карте, имеющая имя, тип,
+//  границы, набор пользовательских свойств (в формате ключ-значение)
+//  и текстурные координаты.
+// Текстурные координаты позволяют связать с объектом спрайт,
+//  использующий основную текстуру карты как источник данных.
 struct TmxObject
 {
     int GetPropertyInt(std::string name);
@@ -22,9 +27,13 @@ struct TmxObject
 	sf::Sprite sprite;
 };
 
+// В картах TMX слой - это набор тайлов (спрайтов),
+//  из которых складывается ландшафт карты.
+// Слоёв может быть несколько, что позволяет нарисовать,
+//  например, слой травы поверх слоя земли.
 struct TmxLayer
 {
-    int opacity;
+    int opacity = 0;
     std::vector<sf::Sprite> tiles;
 };
 
@@ -41,12 +50,11 @@ public:
     void Draw(sf::RenderTarget &target)const;
 
 private:
-    int m_width;
-    int m_height;
-    int m_tileWidth;
-    int m_tileHeight;
-    int m_firstTileID;
-    sf::FloatRect m_drawingBounds;
+    int m_width = 0;
+    int m_height = 0;
+    int m_tileWidth = 0;
+    int m_tileHeight = 0;
+    int m_firstTileID = 0;
     sf::Texture m_tilesetImage;
     std::vector<TmxObject> m_objects;
     std::vector<TmxLayer> m_layers;
